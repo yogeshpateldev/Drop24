4979bb6ecf84b16574739df0150a84c8a83d1e99
yogeshpateldev <yogeshpatel.in1@gmail.com>
2025-07-28 16:44:44 +0530

    link update

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://drop24-backend.onrender.com',
+  baseURL: 'https://drop24-backend.onrender.com/api',
   withCredentials:false // Adjust if your backend is on a different port
 });
 
