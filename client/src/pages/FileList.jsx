78077870c196e2ae9f7e49d12060bd35a51ee3fa
yogeshpateldev <yogeshpatel.in1@gmail.com>
2025-08-02 17:31:09 +0530

    change download link 2

--- client/src/pages/FileList.jsx
+++ client/src/pages/FileList.jsx
@@ -47,11 +47,7 @@ function FileList({ files, setFiles }) {
               <div className="flex justify-between items-center mt-2">
                 {/* ✅ Download Button */}
                 <a
-                  href={
-                    file.url.includes('/raw/')
-                      ? file.url.replace('/raw/upload/', `/raw/upload/fl_attachment:${file.originalname}/`)
-                      : file.url.replace('/upload/', `/raw/upload/fl_attachment:${file.originalname}/`)
-                  }
+                  href={`https://res.cloudinary.com/dwiptzkwk/raw/upload/fl_attachment:${file.originalname}/${file.public_id}`}
                   download={file.originalname}
                   className="bg-green-500 hover:bg-green-600 text-white text-xs px-2 py-1 rounded"
                 >
