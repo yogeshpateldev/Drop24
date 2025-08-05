a24fbeb34e583e27bf03ac24f29fdc3eb36b9f5f
yogeshpateldev <yogeshpatel.in1@gmail.com>
2025-08-05 18:02:57 +0530

    mongo uri

--- server/server.js
+++ server/server.js
@@ -59,7 +59,7 @@ app.use(cors({
 
 app.use(express.json({ limit: '50mb' }));
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
